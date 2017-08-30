82de1137ccd0bb6cfedaa123cf143983d6d97140
monikagoyat25 <monika.5.singh@niit-tech.com>
2017-08-30 19:52:43 +0530

    issues resolved

--- DataLogic.cs
+++ DataLogic.cs
@@ -1,107 +1,142 @@
 ﻿using System;
 using System.IO;
+using System.Text;
 
-namespace CrimeDataFinalCSVtoJSON
+namespace CSVtoJSONChicagoCrimeData
 {
-    class Program
+    public class DataLogic
     {
-        static void Main(string[] args)
+        String[] Heading = new String[6] { "Year", "Over$500", "Under$500", "Year", "Arrest", "NotArrest" };
+        String[] pieheading = new String[4] { "IndexCrime", "NonIndexCrime", "VoilentCrime", "PropertyCrime" };
+        int[] TheftPerYearUnder500 = new int[16];
+        int[] TheftPerYearOver500 = new int[16];
+        int[] Assault_Arrest_peryear = new int[16];
+        int[] Assault_notArrest_peryear = new int[16];
+        int[] CrimeCategory = new int[4];
+        StringBuilder file = new StringBuilder();
+        StringBuilder file1 = new StringBuilder();
+        StringBuilder file2 = new StringBuilder();
+        public void CsvToJson()
         {
             FileStream freader = new FileStream(@"C:\Users\Training\Downloads\crimedata.csv", FileMode.Open, FileAccess.ReadWrite);
             StreamReader reader = new StreamReader(freader);
-            String[] Heading = new String[6] { "Year", "Over$500", "Under$500", "Year", "Arrest", "NotArrest" };
-            String[] pieheading = new String[4] { "IndexCrime", "NonIndexCrime", "VoilentCrime", "PropertyCrime" };
-            int[] TheftPerYearUnder500 = new int[16];
-            int[] TheftPerYearOver500 = new int[16];
-            int[] Assault_Arrest_peryear = new int[16];
-            int[] Assault_notArrest_peryear = new int[16];
-            int[] CrimeCategory = new int[4];
             string line = string.Empty;
-            int x = 0, count = 0;
-
+            int x = 0;
             while ((line = reader.ReadLine()) != null)
             {
-                count++;
-                string[] values = line.Split('"');
-                if (values.Length > 1)
+                if (line.Contains("ASSAULT") || line.Contains("OVER $500") || line.Contains("$500 AND UNDER")|| line.Contains("2015"))
                 {
-                    for (int i = 1; i < values.Length; i += 2)
+                    string[] values = line.Split('"');
+                    if (values.Length > 1)
                     {
-                        values[i] = values[i].Replace(",", " ");
+                        for (int i = 1; i < values.Length; i += 2)
+                        {
+                            values[i] = values[i].Replace(",", " ");
+                        }
                     }
-                }
-                line = string.Empty;
-                foreach (var a1 in values)
-                {
-                    line += a1;
-                }
-                string[] val = line.Split(',');
-                if (count == 1)
-                    continue;
-                else
-                {
+                    line = string.Empty;
+                    foreach (var a1 in values)
+                    {
+                        line += a1;
+                    }
+                    string[] val = line.Split(',');
+
                     Int32 year;
                     Int32.TryParse(val[17], out year);
                     if (year >= 2001 && year <= 2016)
                     {
-                        x = year - 2001;
-                        int one = (val[6] == "OVER $500") ? TheftPerYearOver500[x] += 1 : ((val[6] == "$500 AND UNDER") ? TheftPerYearUnder500[x] += 1 : 0);
-                        int two = ((val[5] == "ASSAULT") && (val[8].ToLower() == "true")) ? Assault_Arrest_peryear[x] += 1 : ((val[5] == "ASSAULT" && val[8].ToLower() == "false") ? Assault_notArrest_peryear[x] += 1 : 0);
+                        if (val[6] == "OVER $500")
+                        {
+                            x = year - 2001;
+                            TheftPerYearOver500[x] += 1;
+                        }
+                        else if (val[6] == "$500 AND UNDER")
+                        {
+                            x = year - 2001;
+                            TheftPerYearUnder500[x] += 1;
+                        }
+
+                        if ((val[5] == "ASSAULT") && (val[8].ToLower() == "true"))
+                        {
+                            x = year - 2001;
+                            Assault_Arrest_peryear[x] += 1;
+                        }
+                        else if (val[5] == "ASSAULT" && val[8].ToLower() == "false")
+                        {
+                            x = year - 2001;
+                            Assault_notArrest_peryear[x] += 1;
+                        }
                     }
                     if (year == 2015)
                     {
                         if (val[14] == "01A" || val[14] == "02" || val[14] == "03" || val[14] == "04A" || val[14] == "04B" || val[14] == "05" || val[14] == "06" || val[14] == "07" || val[14] == "09")
+                        {
                             CrimeCategory[0] += 1;
+                        }
                         if (val[14] == "01B" || val[14] == "08A" || val[14] == "08B" || val[14] == "10" || val[14] == "11" || val[14] == "12" || val[14] == "13" || val[14] == "14" || val[14] == "15" || val[14] == "16" || val[14] == "17" || val[14] == "18" || val[14] == "19" || val[14] == "20" || val[14] == "22" || val[14] == "24" || val[14] == "26")
+                        {
                             CrimeCategory[1] += 1;
+                        }
                         if (val[14] == "01A" || val[14] == "02" || val[14] == "03" || val[14] == "04A" || val[14] == "04B")
+                        {
                             CrimeCategory[2] += 1;
+                        }
                         if (val[14] == "05" || val[14] == "06" || val[14] == "07" || val[14] == "09")
+                        {
                             CrimeCategory[3] += 1;
+                        }
                     }
                 }
             }
             freader.Flush();
             reader.Dispose();
-
+        }
+        public void WriteData()
+        {
             // BAR CHART  
             FileStream fwriter = new FileStream(@"data\barchartdata.json", FileMode.OpenOrCreate, FileAccess.ReadWrite);
             StreamWriter writer = new StreamWriter(fwriter);
-            writer.WriteLine("[");
+            file.AppendFormat("[");
             for (int p = 0; p < 16; p++)
             {
-                int a1 = p + 2001;
-                writer.WriteLine("{ \n \"" + Heading[0] + "\":\"" + a1 + "\" , \n \"" + Heading[1] + "\":\"" + TheftPerYearOver500[p] + "\" , \n \"" + Heading[2] + "\":\"" + TheftPerYearUnder500[p] + "\"\n }");
+                int a = p + 2001;
+                file.Append("{ \n").AppendFormat("\"{0}\":\"{1}\"", Heading[0], a).Append(',').Append('\n').AppendFormat("\"{0}\":\"{1}\"", Heading[1], TheftPerYearOver500[p]).Append(',').Append('\n').AppendFormat("\"{0}\":\"{1}\"", Heading[2], TheftPerYearUnder500[p]).Append("\n }");
                 if (p < 15)
-                    writer.Write(',');
+                    file.Append(',');
+                
+                file.Append('\n');
             }
-            writer.WriteLine(" ]");
+            file.Append("]");
+            writer.WriteLine(file);
             fwriter.Flush(); writer.Flush(); writer.Dispose();
             // MULTISERIES LINE CHART
             FileStream fwriter1 = new FileStream(@"data\linechartdata.json", FileMode.OpenOrCreate, FileAccess.ReadWrite);
             StreamWriter writer1 = new StreamWriter(fwriter1);
-            writer1.WriteLine("{\n\"Assault\": [");
+            file1.AppendFormat("[");
             for (int p = 0; p < 16; p++)
             {
-                int a2 = p + 2001;
-                writer1.WriteLine("{ \n \"" + Heading[3] + "\":\"" + a2 + "\" , \n \"" + Heading[4] + "\":\"" + Assault_Arrest_peryear[p] + "\" , \n \"" + Heading[5] + "\":\"" + Assault_notArrest_peryear[p] + "\"\n }");
+                int a = p + 2001;
+                file1.Append("{ \n").AppendFormat("\"{0}\":\"{1}\"", Heading[3], a).Append(',').Append('\n').AppendFormat("\"{0}\":\"{1}\"", Heading[4], Assault_Arrest_peryear[p]).Append(',').Append('\n').AppendFormat("\"{0}\":\"{1}\"", Heading[5], Assault_notArrest_peryear[p]).Append("\n }");
                 if (p < 15)
-                    writer1.Write(',');
+                    file1.Append(',');
+                
+                file1.Append('\n');
             }
-            writer1.WriteLine(" ] \n }");
+            file1.Append("]");
+            writer1.WriteLine(file1);
             fwriter1.Flush(); writer1.Flush(); writer1.Dispose();
             // Pie Chart
             FileStream fwriter2 = new FileStream(@"data\piechartdata.json", FileMode.OpenOrCreate, FileAccess.ReadWrite);
             StreamWriter writer2 = new StreamWriter(fwriter2);
-            writer2.WriteLine("[");
+            file2.Append("[");
             for (int p = 0; p < 4; p++)
             {
-                writer2.WriteLine("{ \n \"Category\":\"" + pieheading[p] + "\" , \n \"Value\":\"" + CrimeCategory[p] + "\"\n } ");
-                if (p < 3) writer2.Write(',');
+                file2.Append("{ \n").AppendFormat("\"Category\":\"{0}\"", pieheading[p]).Append(" ,\n ").AppendFormat("\"Value\":\"{0}\"", CrimeCategory[p]).Append("\n }");
+                if (p < 3) file2.Append(',');
             }
-            writer2.WriteLine("\n]");
+            file2.Append("\n]");
+            writer2.WriteLine(file2);
             fwriter2.Flush(); writer2.Flush(); writer2.Dispose();
         }
-      
     }
 }
